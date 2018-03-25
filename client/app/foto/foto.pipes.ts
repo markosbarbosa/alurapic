ef0e4ff31e7ef6a621fb2e0d006531aa94c338bb
Marcos Barbosa <barbosa.mg@gmail.com>
2018-03-24 21:03:03 -0300

    Usando pipe para filtrar fotos

---
+++ client/app/foto/foto.pipes.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'filtroPorTitulo',
+})
+export class FiltroPorTitulo implements PipeTransform {
+    
+    transform(fotos, digitado) {
+        digitado = digitado.toLowerCase();
+        return fotos.filter(foto => foto.titulo.toLowerCase().includes(digitado));
+    }
+}
